ace3f059b3022dc5f87dedd9cf8248e7d52bef5b
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-06 13:11:53 -0300

    elimine corroboracion de dato de codigo triangulo

--- triangulo.rb
+++ triangulo.rb
@@ -1,6 +1,5 @@
 # Haciendo uso de .times o el ciclo for dibuja un triángulo con el carácter que consideres más idóneo, pueden ser asteriscos (*) o numerales (#).
 cantidad_asteriscos=ARGV[0].to_i
-puts cantidad_asteriscos
 def triangulo(cantidad_asteriscos)
     if cantidad_asteriscos==0
         puts "ERROR-TRIANGULO:\n RECUERDA: debes ingresar un\n argumento en la linea de codigo para\n poder dibujar un triangulo\n EJEMPLO: 'ruby main.rb 5'\n o\n 'ruby triangulo.rb 5'"
